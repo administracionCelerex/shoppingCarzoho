424e3e76e4851b00b921a0a402d8be3affe32307
luissevilla <luissevilla111@gmail.com>
2022-08-01 21:07:06 -0500

    added some html and css

---
+++ app/main.js
@@ -0,0 +1,22 @@
+const shopCar = {
+  items: [
+    /* {
+      id: "123",
+      nameItem: "Celuar",
+      quantity: 3,
+      individualPrice: 300,
+    },
+    {
+      id: "456",
+      nameItem: "MAC",
+      quantity: 1,
+      individualPrice: 300,
+    }, */
+  ],
+  totalPrice: 0,
+  totalItems: 0,
+};
+
+const agregarCarrito = (item) => {
+  console.log("Hola");
+};
